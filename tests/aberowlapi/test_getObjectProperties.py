483cee8661c9eb87ec461c0c2c3f0919187b2ac9
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2025-04-13 22:24:13 +0300

    🚧 Generating unittests

---
+++ tests/aberowlapi/test_getObjectProperties.py
@@ -0,0 +1,35 @@
+from aberowlapi.server_manager import OntologyServerManager
+from aberowlapi.util import release_port
+import gevent
+gevent.monkey.patch_all()
+## KEEP THE 'import requests' after the 'import gevent' and 'gevent.monkey.patch_all()'
+from unittest import TestCase
+import requests
+import threading
+import time
+
+import os
+# gevent.config.loop = "default"
+
+class TestHealth(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.BASE_URL = "http://localhost:8080"
+        cls.manager = OntologyServerManager(os.path.abspath("data/pizza.owl"))
+        cls.server_greenlet = gevent.spawn(cls.manager.run)
+        time.sleep(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server_greenlet.kill(block=True, timeout=2)  # Kill the greenlet
+
+        release_port(8080)
+
+        
+    def test_getObjectProperties(cls):
+        response = requests.get(f"{cls.BASE_URL}/api/getObjectProperties.groovy")
+        assert response.status_code == 200
+        assert "ok" in str(response.content)
+
+
