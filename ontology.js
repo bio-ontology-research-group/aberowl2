eafd7ed716fbd73116be1354e3a46c0ecad7ce6b
Robert Hoehndorf <robert.hoehndorf@kaust.edu.sa>
2025-07-10 14:57:39 +0300

    fix: Add space after closing tag in ontology.js

--- ontology.js
+++ ontology.js
@@ -411,7 +411,8 @@ Alpine.data('ontologyApp', () => ({
             .replace(/id='man-keyword'/g, "class='owl-quantifier'")
             .replace(/id='man-and'/g, "class='owl-operator'")
             .replace(/id='man-or'/g, "class='owl-operator'")
-            .replace(/id='man-not'/g, "class='owl-operator'");
+            .replace(/id='man-not'/g, "class='owl-operator'")
+	    .replace(/>/g, '> ');
     },
     
   executeDLQuery(owlClass, queryType, labels = true) {
